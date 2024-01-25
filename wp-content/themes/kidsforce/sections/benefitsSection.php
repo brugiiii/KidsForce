c49a869b50a9826c2ec54de9dc800fb99200fce0
Eduard <edyabrug91@gmail.com>
2024-01-25 17:48:13 +0200

    Ще трохи правок

--- wp-content/themes/kidsforce/sections/benefitsSection.php
+++ wp-content/themes/kidsforce/sections/benefitsSection.php
@@ -3,9 +3,7 @@
         <h2 class="benefits-title section-title">
             <?= the_field('benefits_title'); ?>
         </h2>
-        <p class="benefits-text">
-            <?= the_field('benefits_undertitle'); ?>
-        </p>
+        <?= the_field('benefits_undertitle'); ?>
         <?= the_field('benefits_list'); ?>
     </div>
 </section>
