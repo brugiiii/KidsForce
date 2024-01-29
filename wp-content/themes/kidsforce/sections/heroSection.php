075aa3b4fc407d86797f41d1b3ff69934617f8bb
Eduard <edyabrug91@gmail.com>
2024-01-30 00:28:03 +0200

    commit

--- wp-content/themes/kidsforce/sections/heroSection.php
+++ wp-content/themes/kidsforce/sections/heroSection.php
@@ -1,4 +1,3 @@
-
 <section class="hero position-relative overflow-x-visible">
     <div class="container">
         <h1 class="hero-title">
@@ -7,12 +6,12 @@
         <p class="hero-text fw-bold">
             <?= the_field('hero_undertitle'); ?>
         </p>
-        <a href="#cta" class="hero-button text-center d-inline-block border-style">
+        <a href="#cta" class="hero-button d-inline-block border-style">
             <?= the_field('hero_button'); ?>
         </a>
     </div>
     <div class="hero-thumb video-thumb position-absolute top-50 start-50 translate-middle w-100 h-100 object-fit-cover z-n1">
-        <video src="<?= get_field('hero_movie'); ?>" loop muted autoplay playsinline class="d-none d-lg-block"></video>
-        <video src="<?= get_field('hero_movie_mob'); ?>" loop muted autoplay playsinline class="d-lg-none"></video>
+        <video src="<?= get_field('hero_movie'); ?>" loop muted autoplay class="d-none d-lg-block"></video>
+        <video src="<?= get_field('hero_movie_mob'); ?>" playsinline autoplay loop muted class="d-lg-none"></video>
     </div>
 </section>
